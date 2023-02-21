2139c37c24f0f4764718cc2fac94081998178ca8
Gentrit Halili <gentrithalili11@gmail.com>
2023-02-21 11:08:54 +0100

    Fix css not loading

--- src/components/PromiseConfirmationModal/PromiseConfirmationModal.tsx
+++ src/components/PromiseConfirmationModal/PromiseConfirmationModal.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react'
-import './PromiseConfirmationModal.css'
 import { PromiseConfirmationData } from '../../types'
+import './PromiseConfirmationModal.css'
 
 export default function PromiseConfirmationModal(
   props: PropsWithChildren<PromiseConfirmationData>,
